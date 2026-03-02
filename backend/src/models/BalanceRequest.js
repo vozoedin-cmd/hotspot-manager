d9191dddb194d4afc6b568d28092c944f1325949
Hotspot Admin <admin@hotspot.com>
2026-03-01 23:13:07 -0600

    fix: correct database import path in BalanceRequest model

--- backend/src/models/BalanceRequest.js
+++ backend/src/models/BalanceRequest.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database/connection');
+const { sequelize } = require('../config/database');
 
 const BalanceRequest = sequelize.define(
   'BalanceRequest',
